43f3dfc40217a43c5bb1104ee3a404f90a2ebfed
Michal Kacprzak <michal.kacprzak999@gmail.com>
2021-05-29 23:11:19 +0200

    create neutral network for iris dataset classification

---
+++ ML-learning-visualisation/NN.py
@@ -0,0 +1,68 @@
+import torch
+import torch.nn
+import torch.nn.functional
+
+
+class NN:
+    def __init__(self):
+
+        self.model = torch.nn.Sequential(
+            torch.nn.Linear(4, 20),
+            torch.nn.ReLU(),
+            torch.nn.Linear(20, 20),
+            torch.nn.Linear(20, 3),
+            torch.nn.Softmax(dim=1)
+        )
+        self.loss_vector = []
+        self.criterion = torch.nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=0.01)
+
+        self.weights = []
+        self.bias = []
+        self.layers = get_layers(self.model)
+
+        weights, bias = extract_weight_bias(self.model)
+        self.weights.append(weights)
+        self.bias.append(bias)
+
+    def train(self, train_X, train_y):
+
+        for epoch in range(1000):
+            self.optimizer.zero_grad()
+            out = self.model(train_X)
+            loss = self.criterion(out, train_y)
+            loss.backward()
+            weights, bias = extract_weight_bias(self.model)
+            self.weights.append(weights)
+            self.bias.append(bias)
+            self.optimizer.step()
+
+            if epoch % 100 == 0:
+                print('number of epoch', epoch, 'loss', loss.item())
+
+            self.loss_vector.append(loss.item())
+
+
+def extract_weight_bias(model):
+    weights = []
+    bias = []
+    for name, param in model.named_parameters():
+        param = param.detach().numpy()
+        if 'weight' in name:
+            weights.append(param)
+        if 'bias' in name:
+            bias.append(param)
+    return weights, bias
+
+
+def get_layers(model):
+    layers = []
+    for name, param in model.named_parameters():
+        if 'weight' in name and len(layers) == 0:
+            param = param.detach().numpy()
+            layers.append(param.shape[1])
+        if 'bias' in name:
+            param = param.detach().numpy()
+
+            layers.append(param.shape[0])
+    return layers
---
+++ ML-learning-visualisation/main.py
@@ -0,0 +1,37 @@
+import torch
+from torch.autograd import Variable
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+
+from NN import NN
+
+
+def main():
+    # load iris dataset
+    iris = load_iris()
+
+    # create test/train data
+    datasets = train_test_split(iris.data, iris.target, test_size=0.2)
+    train_X, test_X, train_y, test_y = datasets
+    train_X = Variable(torch.Tensor(train_X).float())
+    test_X = Variable(torch.Tensor(test_X).float())
+    train_y = Variable(torch.Tensor(train_y).long())
+    test_y = Variable(torch.Tensor(test_y).long())
+
+    # construct and train network
+    net = NN()
+    net.train(train_X, train_y)
+
+    # test network
+    predict_out = net.model(test_X)
+    _, predict_y = torch.max(predict_out, 1)
+    print("confusion_matrix")
+    print(confusion_matrix(test_y.data, predict_y.data))
+    labels = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+    print(classification_report(test_y.data, predict_y.data, target_names=labels))
+
+
+if __name__ == '__main__':
+    main()
