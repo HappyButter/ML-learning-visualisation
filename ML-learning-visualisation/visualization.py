f272692f23425d56e9426483a5c196193c001bed
mikolajczykb <uncharted.mikolajczyk4@gmail.com>
2021-06-13 15:05:39 +0200

    add docstrings

--- ML-learning-visualisation/visualization.py
+++ ML-learning-visualisation/visualization.py
@@ -13,6 +13,19 @@ WINDOW_WIDTH = 1400
 
 
 def calc_distance_between_nodes(layers: list) -> Tuple[int, list]:
+    """Function calculating distances on x and y axis between nodes
+
+    Parameters
+    ----------
+    layers: List
+        A list containing the amount of nodes in each layer of the neural network
+
+    Returns
+    -------
+    Tuple[int, list]
+        Distance on the x axis between the layers and list of distances between nodes on the y axis in each layer
+
+    """
     amount_of_layers = len(layers) + 2
     dist_x = WINDOW_WIDTH // amount_of_layers
     dist_y = []
@@ -22,6 +35,23 @@ def calc_distance_between_nodes(layers: list) -> Tuple[int, list]:
 
 
 def create_node_position_list(layers: list, dist_x: int, dist_y: list) -> Tuple[list, list]:
+    """Function calculating x and y coordinates of every node in the neural network
+
+    Parameters
+    ----------
+    layers: List
+        A list containing the amount of nodes in each layer of the neural network
+    dist_x: int
+        A int value representing the distance on x axis between each layer
+    dist_y: list
+        A tuple representing the distance on y axis between nodes in every layer
+
+    Returns
+    -------
+    Tuple[list, list]
+        Tuple containing two lists: One for the x axis positions of every node, and one for the y axis positions
+
+    """
     node_position_list_x = []
     distance_counter = 0
     for layer in layers:
@@ -36,6 +66,22 @@ def create_node_position_list(layers: list, dist_x: int, dist_y: list) -> Tuple[
 
 
 def create_edge_position_list(layers: list, node_position_list_x: list, node_position_list_y: list) -> list:
+    """Function calculating x and y coordinates of every edge in the neural network
+
+    Parameters
+    ----------
+    layers: List
+        A list containing the amount of nodes in each layer of the neural network
+    node_position_list_x: list
+        A list containing x axis positions of every node in the network
+    node_position_list_y: list
+        A list of lists containing y axis positions of every node in the network
+
+    Returns
+    -------
+        Position info about every node in the graph
+
+    """
     edge_positions = []
 
     current_pos = layers[0]
@@ -58,6 +104,21 @@ def create_edge_position_list(layers: list, node_position_list_x: list, node_pos
 
 
 def create_bias_list(layers: list, bias: list) -> list:
+    """Function for creating a list of biases
+
+    Parameters
+    ----------
+    layers: list
+        A list specifying the number of nodes in each layer
+    bias: list
+        A list of biases
+
+    Returns
+    -------
+        A list of biases for every node
+
+    """
+
     bias_list = [0 for _ in range(layers[0])]
     for layer_bias in bias:
         bias_list.extend(layer_bias)
@@ -66,6 +127,18 @@ def create_bias_list(layers: list, bias: list) -> list:
 
 
 def create_weights_list(weights: list):
+    """Function for creating a list of weights more suited for our app
+
+    Parameters
+    ----------
+    weights: list
+        List of weights
+
+    Returns
+    -------
+        A list of weights more suited for our rendering function
+
+    """
     weights_list = []
     for weights_layer in weights:
         for weights_from_node in weights_layer:
@@ -75,6 +148,18 @@ def create_weights_list(weights: list):
 
 # weight / 128 -> should be / 2, but we are dividing by larger number to lose float information
 def get_rgb_from_weight(weight: float) -> str:
+    """Function calculating rgb value from a weight float value
+
+    Parameters
+    ----------
+    weight: float
+        A weight value of the edge
+    Returns
+    -------
+    str
+        RGB value in form of a string
+
+    """
     weight += 1
     weight = weight / 128
     weight = 255 - int(weight * 255) * 64 - 40
@@ -84,6 +169,26 @@ def get_rgb_from_weight(weight: float) -> str:
 
 def get_weight_group(min_range: float, max_range: float, weights: list, edge_positions: list, threshold: float) \
         -> Tuple[list, list]:
+    """Function getting positions of nodes whose weights are in the (min_range, max_range) range
+
+    Parameters
+    ----------
+    min_range: float
+        Smallest float value of weight the function accepts
+    max_range: float
+        Largest flaot value of weight the function accepts
+    weights: list
+        List of weights
+    edge_positions: list
+        List of all edges' positions
+    threshold: float
+        Minimum absolute value for the weights
+    Returns
+    -------
+    str
+        RGB value in form of a string
+
+    """
     edge_pos_x = []
     edge_pos_y = []
     for index, weight in enumerate(weights):
@@ -94,6 +199,18 @@ def get_weight_group(min_range: float, max_range: float, weights: list, edge_pos
 
 
 def get_min_weight(weights: list) -> float:
+    """Function for returning minimum weight from list of weights
+
+    Parameters
+    ----------
+    weights: list
+        List of weights
+
+    Returns
+    -------
+        Minimum weight from list
+
+    """
     minimum = 1
     for weight in weights:
         minimum = min(minimum, np.amin(weight))
@@ -101,6 +218,18 @@ def get_min_weight(weights: list) -> float:
 
 
 def get_max_weight(weights: list) -> float:
+    """Function for returning minimum weight from list of weights
+
+    Parameters
+    ----------
+    weights: list
+        List of weights
+
+    Returns
+    -------
+        Maximum weight from list
+
+    """
     maximum = -1
     for weight in weights:
         maximum = max(maximum, np.amax(weight))
@@ -108,6 +237,24 @@ def get_max_weight(weights: list) -> float:
 
 
 def validate_epoch_number_to_draw(epoch_number: int, epoch_number_to_draw: int, max_number: int = 50) -> int:
+    """Function validating epochs number that we want to draw
+
+    Parameters
+    ----------
+    epoch_number: int
+        Int value that specifies the amount of epochs our network will go through
+
+    epoch_number_to_draw: int
+        Int value that specifies the epochs number we want to draw
+
+    max_number: int
+        The maximum amount of epochs we want to render
+
+    Returns
+    -------
+        Epoch number within our boundaries
+
+    """
     if epoch_number_to_draw < 1:
         return max_number if epoch_number > max_number else epoch_number
     if epoch_number_to_draw > max_number:
@@ -116,8 +263,25 @@ def validate_epoch_number_to_draw(epoch_number: int, epoch_number_to_draw: int, 
     return epoch_number_to_draw
 
 
-def visualise_ML(layers: list, bias: list, weights, epoch_number, epoch_number_to_draw: int = -1,
+def visualise_ML(layers: list, bias: list, weights: list, epoch_number: int, epoch_number_to_draw: int = -1,
                  threshold: float = 0.05):
+    """Our main function for visualizing the neural network learning process
+
+    Parameters
+    ----------
+    layers: list
+        List containing the amount of nodes in each layer
+    bias: list
+        List of biases
+    weights: list
+        List of weights
+    epoch_number: int
+        Amount of epochs for the NN to process
+    epoch_number_to_draw: int
+        Amount of epochs we want to draw
+    threshold: float
+        Minimum absolute value for the weights
+    """
     epoch_number_to_draw = validate_epoch_number_to_draw(epoch_number, epoch_number_to_draw, max_number=100)
 
     # Create figure
